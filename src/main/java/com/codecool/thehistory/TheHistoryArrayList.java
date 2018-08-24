1f1929e2e6037e35e6b7886a7a53cc1e3db6365a
MattHowdy <mate.hodi9@gmail.com>
2018-08-24 17:01:04 +0200

    ArrayList exericeses

--- src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
@@ -1,9 +1,8 @@
 package com.codecool.thehistory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
+//import java.util.Arrays;
+
 
 public class TheHistoryArrayList implements TheHistory {
     /**
@@ -14,32 +13,124 @@ public class TheHistoryArrayList implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+
+        List<String> items = Arrays.asList(text.split("\\W+"));
+        wordsArrayList.addAll(items);
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        // 65
+        Iterator<String> itrArray = wordsArrayList.iterator();
+        while(itrArray.hasNext()){
+            if(wordToBeRemoved.equals(itrArray.next())){
+                itrArray.remove();
+            }
+        }
+
+        // 959 ms
+//        for(int i = 0; i < wordsArrayList.size(); i++ ){
+//            if(wordsArrayList.get(i).equals(wordToBeRemoved)){
+//                wordsArrayList.remove(wordsArrayList.get(i));
+//            }
+//        }
+
+
+//        2115 ms
+//        while(wordsArrayList.indexOf(wordToBeRemoved) > 0){
+//            wordsArrayList.remove(wordsArrayList.indexOf(wordToBeRemoved));
+//        }
+
+
+
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArrayList.size();
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsArrayList.clear();
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+
+        //34 ms
+        int len = wordsArrayList.size();
+
+        for(int i = 0; i < len; i++){
+            if(wordsArrayList.get(i).equals(from)){
+                wordsArrayList.set(i, to);
+            }
+        }
+//
+
+
+        // 73 ms
+//        ListIterator<String> iterator = wordsArrayList.listIterator();
+//        while (iterator.hasNext()) {
+//            String next = iterator.next();
+//            if (next.equals(from)) {
+//                iterator.set(to);
+//            }
+//        }
+
+        // 194 ms
+//        List<String> newList = new ArrayList<String>();
+//        for(int i = 0; i < wordsArrayList.size(); i++)
+//        {
+//            if(wordsArrayList.get(i).contains(from))
+//            {
+//                newList.add(wordsArrayList.get(i).replace(from, to));
+//                //someList.set(i, someList.get(i).replace(someString, otherString));
+//            } else {
+//
+//                // If it not contains `someString`, add it as it is to newList
+//                newList.add(wordsArrayList.get(i));
+//            }
+//        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+        int fulLen = wordsArrayList.size();
+        int fromLen = fromWords.length;
+        int toLen = toWords.length;
+        int fixedIndex = 0;
+        boolean match = false;
+        int removeFrom = 0;
+
+
+        try{
+            for(int i = 0; i < fulLen; i++){
+
+                if(wordsArrayList.get(i).equals(fromWords[fixedIndex])){
+                    removeFrom = i;
+
+                    for(int j = 1; j < fromLen; j++){
+                        if(wordsArrayList.get(i+1).equals(fromWords[j])){
+                            match = true;
+
+                    }
+
+                }
+                }
+                if(match){
+                    for (int k = removeFrom; k < fromLen; k++){
+                        wordsArrayList.set(k, toWords[k]);
+                    }
+                }
+            }
+        }
+        catch (IndexOutOfBoundsException e){}
     }
 
     @Override
